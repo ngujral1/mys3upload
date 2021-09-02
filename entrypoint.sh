fb6757ce82c07e1913d49791ec90f9ef34c3cb9a
ngujral1 <gujral.nitin@gmail.com>
2021-09-02 12:05:12 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,4 @@
 
 set -e
 
-# Sync using our dedicated profile and suppress verbose messages.
-# All other flags are optional via the `args:` directive.
 sh -c "aws s3 cp ${INPUT_SOURCE} ${INPUT_DEST}"
